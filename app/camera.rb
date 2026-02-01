b5f28e416791cf4876f0d7b54648d8942adbb8c5
eejit <eejitgames@gmail.com>
2026-02-01 20:26:42 +0000

    updated camera ivar names

--- app/camera.rb
+++ app/camera.rb
@@ -2,18 +2,18 @@ class Camera
   attr_accessor :x, :y
 
   def initialize( shake_amplitude: 100, shake_freq_x: 0.5, shake_freq_y: 0.7, shake_decay: 0.95, shake_max: 0.6 )
-    @amplitude = shake_amplitude
-    @frequency_x = shake_freq_x
-    @frequency_y = shake_freq_y
-    @decay = shake_decay
+    @shake_amplitude = shake_amplitude
+    @shake_frequency_x = shake_freq_x
+    @shake_frequency_y = shake_freq_y
+    @shake_decay = shake_decay
     @shake_max = shake_max
     reset
   end
 
   def reset
+    @shake = 0.0
     @x = 0
     @y = 0
-    @shake = 0.0
   end
 
   def add_shake( amount )
@@ -24,10 +24,10 @@ class Camera
   def update_shake( t )
     return if @shake.zero?
 
-    s = [ @amplitude * @shake**2, @amplitude ].min
-    @x = ( Math.sin( t * @frequency_x ) * s ).floor
-    @y = ( Math.cos( t * @frequency_y ) * s ).floor
-    @shake *= @decay
+    s = [ @shake_amplitude * @shake**2, @shake_amplitude ].min
+    @x = ( Math.sin( t * @shake_frequency_x ) * s ).floor
+    @y = ( Math.cos( t * @shake_frequency_y ) * s ).floor
+    @shake *= @shake_decay
     reset if @shake < 0.07
   end
 end
