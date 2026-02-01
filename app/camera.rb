536102117d57171be19f5b7de821515f2c3cc88e
eejit <eejitgames@gmail.com>
2026-02-01 20:14:35 +0000

    indentation fixes

--- app/camera.rb
+++ app/camera.rb
@@ -1,33 +1,33 @@
 class Camera
-    attr_accessor :x, :y
+  attr_accessor :x, :y
 
-    def initialize( shake_amplitude: 100, shake_freq_x: 0.5, shake_freq_y: 0.7, shake_decay: 0.95, shake_max: 0.6 )
-        @amplitude = shake_amplitude
-        @frequency_x = shake_freq_x
-        @frequency_y = shake_freq_y
-        @decay = shake_decay
-        @shake_max = shake_max
-        reset
-    end
+  def initialize( shake_amplitude: 100, shake_freq_x: 0.5, shake_freq_y: 0.7, shake_decay: 0.95, shake_max: 0.6 )
+    @amplitude = shake_amplitude
+    @frequency_x = shake_freq_x
+    @frequency_y = shake_freq_y
+    @decay = shake_decay
+    @shake_max = shake_max
+    reset
+  end
 
-    def reset
-        @x = 0
-        @y = 0
-        @shake = 0.0
-    end
+  def reset
+    @x = 0
+    @y = 0
+    @shake = 0.0
+  end
 
-    def add_shake( amount )
-        @shake += amount
-        @shake = @shake_max if @shake > @shake_max
-    end
+  def add_shake( amount )
+    @shake += amount
+    @shake = @shake_max if @shake > @shake_max
+  end
 
-    def update_shake( t )
-        return if @shake.zero?
+  def update_shake( t )
+    return if @shake.zero?
 
-        s = [ @amplitude * @shake**2, @amplitude ].min
-        @x = ( Math.sin( t * @frequency_x ) * s ).floor
-        @y = ( Math.cos( t * @frequency_y ) * s ).floor
-        @shake *= @decay
-        reset if @shake < 0.07
-    end
+    s = [ @amplitude * @shake**2, @amplitude ].min
+    @x = ( Math.sin( t * @frequency_x ) * s ).floor
+    @y = ( Math.cos( t * @frequency_y ) * s ).floor
+    @shake *= @decay
+    reset if @shake < 0.07
+  end
 end
--- app/game_emulation.rb
+++ app/game_emulation.rb
@@ -12,28 +12,28 @@ ZOOMED_WIDTH = GAME_WIDTH * ZOOM
 ZOOMED_HEIGHT = GAME_HEIGHT * ZOOM
 
 def self.boot args
-    args.state = {}
+  args.state = {}
 end
 
 def self.tick args
-    args.outputs.background_color = [ 0, 0, 0 ]
-    args.outputs[ :room ].set w: GAME_WIDTH,
+  args.outputs.background_color = [ 0, 0, 0 ]
+  args.outputs[ :room ].set w: GAME_WIDTH,
                             h: GAME_HEIGHT,
                             background_color: [ 73, 81, 105, 255 ]
 
-    $game ||= Game.new
-    $game.args = args
-    $game.tick
+  $game ||= Game.new
+  $game.args = args
+  $game.tick
 
-    args.outputs.sprites << {
-        x: $game.camera.x * ZOOM,
-        y: $game.camera.y * ZOOM,
-        w: ZOOMED_WIDTH,
-        h: ZOOMED_HEIGHT,
-        path: :room
-        }
+  args.outputs.sprites << {
+      x: $game.camera.x * ZOOM,
+      y: $game.camera.y * ZOOM,
+      w: ZOOMED_WIDTH,
+      h: ZOOMED_HEIGHT,
+      path: :room
+      }
 end
 
 def self.reset args
-    $game = nil
+  $game = nil
 end
